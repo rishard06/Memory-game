a27fc348910be7cdcce60185ea9d15a88c18a63f
richard06 <richardmacabus06@gmail.com>
2024-12-21 22:58:08 +0800

    all base file

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './styles/index.css'
+// import App from './App.jsx'
+import Header from './components/header.jsx'
+import Card from './components/card.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <Header />
+    <Card />
+  </StrictMode>,
+)
