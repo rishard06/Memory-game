eed21177c27cf4a937a014b0de6b8cc50a1ee59d
richard06 <richardmacabus06@gmail.com>
2024-12-24 00:25:50 +0800

    just add the Card component to main

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 // import App from './App.jsx'
 import Header from './components/header.jsx'
-import Card from './components/cards.jsx'
+import Card from './components/grid-card.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
